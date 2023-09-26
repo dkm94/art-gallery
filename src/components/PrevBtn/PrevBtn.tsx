e5092c7c63b849b3f6d7c5030053593d59358d79
dkm94 <diane.mpacko@gmail.com>
2023-09-26 18:30:29 +0200

    - updated prev and next btns design for medium screens

--- src/components/PrevBtn/PrevBtn.tsx
+++ src/components/PrevBtn/PrevBtn.tsx
@@ -40,7 +40,7 @@ const PrevBtn = ({
       style={{ position: mode === "selected" ? "absolute" : "unset", top: mode === "selected" ? "20%" : ""} } 
     >
       <button
-        className={`prev-btn ${slidePrev ? "prev-btn-animation" : ""}${disablePrev ? "disable-btn" : ""}`} 
+        className={`prev-btn ${slidePrev ? "prev-btn-animation" : ""} ${disablePrev ? "disable-btn" : ""}`} 
         onClick={handleClick} 
         disabled={disablePrev}
       >{text}</button>
